97203e1f53f635aeec1abaf06b0824435c988397
mason <mason.yao@outlook.com>
2023-07-14 15:54:15 +1000

    feat: add migrate-mongo and initial data for db upload

---
+++ migrations/20230710062740-init-rss-feeds.js
@@ -0,0 +1,41 @@
+const { ObjectId } = require("mongoose").Types;
+
+module.exports = {
+  async up(db) {
+    const RSSFeed1 = new ObjectId();
+    const RSSFeed2 = new ObjectId();
+    await db.collection("RSSFeeds").insertMany([
+      {
+        _id: RSSFeed1,
+        name: "news btc",
+        link: "https://www.newsbtc.com/news/ethereum/feed/",
+        lastFetchedTime: new Date(),
+      },
+      {
+        _id: RSSFeed2,
+        name: "hacker news",
+        link: "https://feeds.feedburner.com/TheHackersNews",
+        lastFetchedTime: new Date(),
+      },
+    ]);
+    const subscriber1 = new ObjectId();
+    await db.collection("subscribers").insertMany([
+      {
+        _id: subscriber1,
+        email: "eth.tldr@gmail.com",
+        createdTime: new Date(),
+        lastUpdatedTime: new Date(),
+        isDeleted: false,
+        emailStatus: "unsent",
+      },
+    ]);
+    await db.collection("DailyEmailContent").insertMany([
+      {
+        _id: new ObjectId(),
+        date: new Date(),
+        _rssIds: [RSSFeed1, RSSFeed2],
+        subscribers: [subscriber1],
+      },
+    ]);
+  },
+};
